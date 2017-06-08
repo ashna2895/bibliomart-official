fb7368e43653998655f125cbfa80f772c44a93cc
kittycancode <ashna.abdul2895@gmail.com>
2017-06-08 06:38:32 +0530

    confirm order added, order verification added, orders page added

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,9 +1,25 @@
 $('.add-cart-button').on('click',function(){
-    console.log('here');
     var postData = {
         book_id : $(this).attr('bookId'),
         _csrf_token : $(this).attr('csrfToken'),
-        type : 'add'
+        type : 'AddBook'
+    }
+    postCartData(postData);
+});
+
+$('.remove-cart-button').on('click',function(){
+    var postData = {
+        book_id : $(this).attr('bookId'),
+        _csrf_token : $(this).attr('csrfToken'),
+        type : 'RemoveBook'
+    }
+    postCartData(postData);
+});
+
+$('.place-order-button').on('click',function(){
+    var postData = {
+        _csrf_token : $(this).attr('csrfToken'),
+        type : 'PlaceOrder'
     }
     postCartData(postData);
 });
